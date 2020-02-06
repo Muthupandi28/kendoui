8f241b815c508d4411e2b469905fe7ec4ffbda21
MuthuDs <muthust28@gmail.com>
2020-02-06 18:44:27 +0530

    first commit

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using kendoui.Data;
+using kendoui.Models;
+
+namespace kendoui.Controllers
+{
+    public class PizzasController : Controller
+    {
+        private PizzashopsContext db = new PizzashopsContext();
+
+        // GET: Pizzas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Filter()
+        {
+            var pizzas = from m in db.Pizzas select m;
+            return Json(pizzas, JsonRequestBehavior.AllowGet);
+        }
+        // GET: Pizzas/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizzas.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pizzas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public ActionResult Create([Bind(Include = "pizzaid,pizzaname,rate")] Pizza pizza)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pizzas.Add(pizza);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizzas.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+       
+        public ActionResult Edit(Pizza pizza)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pizza).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizzas.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Delete/5
+        [HttpPost, ActionName("Delete")]
+       
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pizza pizza = db.Pizzas.Find(id);
+            db.Pizzas.Remove(pizza);
+            db.SaveChanges();
+            return Json(pizza, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
